33362e6c745630ea1a12f5722f8928245c0e4bfe
Leonardo2831 <leoferraz1818@gmail.com>
2025-10-09 00:43:00 -0300

    main file javascript to link files js

---
+++ public/javascript/script.js
@@ -0,0 +1,7 @@
+import { initEditable } from './editable.js';
+import { initToggleMenu } from './toggleMenu.js';
+import initStorage from './storage.js';
+
+initToggleMenu();
+initEditable();
+initStorage();
